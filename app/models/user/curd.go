7d16e4a55954592228a2f6ada359dfd71aaf9d18
Lee@ubn <qq416606903@hotmail.com>
2024-04-08 20:28:18 +0800

    fix 显示作者

--- app/models/user/curd.go
+++ app/models/user/curd.go
@@ -16,3 +16,12 @@ func (user *User) Create() (err error) {
 func (user User) Link() string {
 	return route.Name2URL("users.show", "id", user.GetStringID())
 }
+
+// All 获取所有用户数据
+func All() ([]User, error) {
+	var users []User
+	if err := model.DB.Find(&users).Error; err != nil {
+		return users, err
+	}
+	return users, nil
+}
